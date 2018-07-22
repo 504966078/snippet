3c49ced0073df1667fb23b782a73bae327dd1d68
Alexander Efremov <a.efremov@javasmith.org>
2018-07-22 23:16:03 +0200

    Comparing observed and Gaussian distributions (add shebang)

--- R/density/main.R
+++ R/density/main.R
@@ -1,10 +1,21 @@
+#!/usr/bin/R --vanilla -q -f 
+#
 #The data is in the following format Value \t Quantity
-values <- read.csv("data.csv",header=FALSE, sep="\t")
-pm2 <- values$V1/values$V2
-dk <- density(pm2)
-dn <- dnorm(dk$x,mean=mean(pm2),sd=sd(pm2))
+#
+
+tbl <- read.csv("data.csv",header=FALSE, sep="\t")
+dsty <- tbl$V1/tbl$V2
+
+dk <- density(dsty)
+dn <- dnorm(dk$x,mean=mean(dsty),sd=sd(dsty))
+
 pdf("out.pdf")
-plot(dk,col="red", main=c("Kernel method vs Gaussian distribution",paste("data sz=", length(pm2),"mean=",as.integer(mean(pm2)),"variance=",as.integer(sd(pm2)))), xlab="density", ylab="probability")
+plot(dk,
+    col="red",
+    main=c("Kernel method vs Gaussian distribution",paste("data sz=", length(dsty),"mean=",as.integer(mean(dsty)),"variance=",as.integer(sd(dsty)))), 
+    xlab="density", 
+    ylab="probability")
 lines(dk$x,dn,col="blue")
-legend("topright",c("Gaussian","Observed"),text.col=c("blue","red"))
+legend("topright",
+        c("Gaussian","Observed"),text.col=c("blue","red"))
 dev.off()
