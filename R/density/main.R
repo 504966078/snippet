af0ad09e9b20844ad6ceefb89e29dba367216730
Alexander Efremov <a.efremov@javasmith.org>
2018-07-22 17:56:33 +0200

    Comparing observed and Gaussian distributions

--- R/density/main.R
+++ R/density/main.R
@@ -1,8 +1,8 @@
 #The data is in the following format Value \t Quantity
 values <- read.csv("data.csv",header=FALSE, sep="\t")
-pm2=values$V1/values$V2
-dk=density(pm2)
-dn=dnorm(dk$x,mean=mean(pm2),sd=sd(pm2))
+pm2 <- values$V1/values$V2
+dk <- density(pm2)
+dn <- dnorm(dk$x,mean=mean(pm2),sd=sd(pm2))
 pdf("out.pdf")
 plot(dk,col="red", main=c("Kernel method vs Gaussian distribution",paste("data sz=", length(pm2),"mean=",as.integer(mean(pm2)),"variance=",as.integer(sd(pm2)))), xlab="density", ylab="probability")
 lines(dk$x,dn,col="blue")
